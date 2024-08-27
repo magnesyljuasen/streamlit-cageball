6389284d91fd1e56b91a5c5017903a4f2d9ec823
magnesyljuasen <msylju@gmail.com>
2024-08-27 19:06:53 +0200

    oppdatert

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -30,14 +30,15 @@ def get_scoreboard(df):
 
 def analyze_dfs(df1, df2):
     for index, row in df2.iterrows():
-        date_column = row.index[1]  # Assuming the date is in the first column after the index
-        winner = row[date_column]        
-        if winner == 'Lag 1':
-            df1[date_column] = df1[date_column].replace('Lag 1', 1)
-            df1[date_column] = df1[date_column].replace('Lag 2', 0)
-        elif winner == 'Lag 2':
-            df1[date_column] = df1[date_column].replace('Lag 2', 1)
-            df1[date_column] = df1[date_column].replace('Lag 1', 0)
+        for j in range(0, len(df1.columns)):
+            date_column = row.index[j]
+            winner = row[date_column]        
+            if winner == 'Lag 1':
+                df1[date_column] = df1[date_column].replace('Lag 1', 1)
+                df1[date_column] = df1[date_column].replace('Lag 2', 0)
+            elif winner == 'Lag 2':
+                df1[date_column] = df1[date_column].replace('Lag 2', 1)
+                df1[date_column] = df1[date_column].replace('Lag 1', 0)
     return df1
 
 def find_cageball_date(df_participants):
@@ -80,36 +81,64 @@ sidebar_logo = "src/img/av-logo-1.png"
 
 st.logo(sidebar_logo, icon_image=main_body_logo)
 
-st.title('Fantasy Cageball')
+df_participants, df_winner = read_excel()
+df_results = analyze_dfs(df1=df_participants, df2=df_winner)
+today, next_date = find_cageball_date(df_participants)
+df_scoreboard = get_scoreboard(df_results)
 
-tab1, tab2, tab3, tab4 = st.tabs(['Påmelding', 'Resultattavle', 'Data', 'Hvem heier du på?'])
-with tab1:
+
+
+with st.sidebar:
+    st.title('Cageball med Asplan Viak')
+    #st.image("src/img/asplanviak_1.jpg", use_column_width=True)
+    selected_page = st.radio('Navigasjon', options=['Påmelding', 'Poengtavle'])
+
+if selected_page == 'Påmelding':
+    st.title('Påmelding')
+    st.info('⚽ *Mot til å angripe, glød i skoene og respekt på banen!*')
     doodle_link = "https://doodle.com/meeting/organize/id/aMojxY1d"
-    st.markdown(f"[Trykk her for påmelding (doodle)]({doodle_link})")
+    st.markdown(f'''<a target="parent" style="
+                color: white; 
+                background-color: #F6F8F1; 
+                border: none; 
+                transition-duration: 0.4s; 
+                font-weight:600; 
+                font-size: 20px; 
+                border-radius: 15px; 
+                text-align: center; 
+                padding: 1rem; 
+                min-height: 60px; 
+                display: inline-block; 
+                box-sizing: border-box; 
+                width: 100%;
+                " href="{doodle_link}">Meld deg på her!</a>''', unsafe_allow_html=True)
+    c1, c2 = st.columns(2)
+    with c1:
+        st.image('src/img/asplanviak_1.jpg', use_column_width=True)
+    with c2:
+        st.image('src/img/asplanviak_2.jpg', use_column_width=True)
 
-    st.image("src/img/asplanviak_1.jpg", use_column_width=True)
 
-with tab2:
-    df_participants, df_winner = read_excel()
-    df_results = analyze_dfs(df1=df_participants, df2=df_winner)
-    today, next_date = find_cageball_date(df_participants)
-    df_scoreboard = get_scoreboard(df_results)
-    show_scoreboard(df_scoreboard.head(8))
-    st.success(f'Mest verdifulle spiller = {df_scoreboard.head(1).index[0]}', icon='🏅')
+if selected_page == 'Poengtavle':
+    st.title('Poengtavle')
+    st.info('🏆 *Topp 3 - Antall seiere*')
+    show_scoreboard(df_scoreboard.head(3))
+    st.success(f'🏅 AV-M.V.P: **{df_scoreboard.head(1).index[0]}**')
+    st.success(f'🐐 AV-G.O.A.T: ?')
     
-with tab3:
+if selected_page == 'Data':
     st.dataframe(df_participants, use_container_width=True, height=400)
     st.dataframe(df_winner, use_container_width=True)
 
-with tab4:
-    cheering = st.selectbox('Hvem heier du på?', options=df_scoreboard.index.values, index=None, placeholder='Velg en fotballspiller...')
-    if cheering == 'Magne':
-        st.success('Riktig svar!')
-        st.balloons()
-    elif cheering == None:
-        pass
-    else:
-        st.warning('Velg en annen...')
+#if selected_page == 'Hvem heier du på?':
+#    cheering = st.selectbox('Hvem heier du på?', options=df_scoreboard.index.values, index=None, placeholder='Velg en fotballspiller...')
+#    if cheering == 'Magne':
+#        st.success('Riktig svar!')
+#        st.balloons()
+#    elif cheering == None:
+#        pass
+#    else:
+#        st.warning('Velg en annen...')
 
 
 
